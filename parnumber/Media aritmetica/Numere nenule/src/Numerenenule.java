82819f2fc1a17350f09f542ad70760641a2ae1a3
GeorgeGaftonie <44541422+GeorgeGaftonie@users.noreply.github.com>
2018-11-10 19:20:24 +0200

    fifth commit

---
+++ parnumber/Media aritmetica/Numere nenule/src/Numerenenule.java
@@ -0,0 +1,19 @@
+public class Numerenenule {
+	public static void main(String[] args) {
+
+		int[] arr = { 5, 2, 0, 1, 4, 3 };
+		int produs = Produs(arr);
+		System.out.println("Rezultat: " + produs);
+	}
+
+	private static int Produs(int[] arr) {
+		int produs = 1;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0) {
+				continue;
+			}
+			produs *= arr[i];
+		}
+		return produs;
+	}
+}
