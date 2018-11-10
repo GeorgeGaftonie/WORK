bd162cf5b86ac48fdb4ae70aac964902cea9895f
GeorgeGaftonie <44541422+GeorgeGaftonie@users.noreply.github.com>
2018-11-10 19:04:43 +0200

    third commit

---
+++ parnumber/minimnumber/src/Minimnumber.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Minimnumber {
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 4, 5 };
+		Arrays.sort(arr);
+		System.out.println("Minim = " + arr[0]);
+		System.out.println("Maxim = " + arr[arr.length - 1]);
+	}
+
+}
