40a9429b975da3adb684f1afc6660896afccd287
GeorgeGaftonie <44541422+GeorgeGaftonie@users.noreply.github.com>
2018-11-25 14:34:28 +0200

    commit

---
+++ src/Session5/LongestSequence.java
@@ -0,0 +1,16 @@
+package Session5;
+
+import java.util.Arrays;
+
+public class LongestSequence {
+	private static void main(String[] args) {
+		 int arr[] = {1, 1, 2, 3,  5, 6, 9};
+		 int arr1[] = {3, 4, 6, 7, 8, 9, 2};	
+		 int longestSeq(int[] sir) {
+			 ({1, 1, 2, 3,  5, 6, 9});
+			 
+		 }
+		 
+		 
+	}
+}
